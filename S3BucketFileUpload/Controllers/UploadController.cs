823cca28b0c708357af15c51f8f4ebc4f0d3bde7
Jon Preece <jonpreece@rentalcars.com>
2018-12-12 15:55:05 +0000

    Get object WIP

--- S3BucketFileUpload/Controllers/UploadController.cs
+++ S3BucketFileUpload/Controllers/UploadController.cs
@@ -15,7 +15,6 @@ using Microsoft.Net.Http.Headers;
 
 namespace S3BucketFileUpload.Controllers
 {
-
     [Route("api/[controller]")]
     public class UploadController : Controller
     {
@@ -26,13 +25,46 @@ namespace S3BucketFileUpload.Controllers
             _hostingEnvironment = hostingEnvironment;
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var keyName = "b1d173c7-4dae-469e-9fdf-a5b999ca7f79/osggsl1l.4wk";
+
+            // Build the request with the bucket name and the keyName (name of the file)
+            var request = new GetObjectRequest
+            {
+                BucketName = "ydkaws.com",
+                Key = keyName
+            };
+
+            using (var client = new AmazonS3Client(RegionEndpoint.USEast1))
+            {
+                using (var response = await client.GetObjectAsync(request))
+                using (var responseStream = response.ResponseStream)
+                using (var reader = new StreamReader(responseStream))
+                {
+                    var title = response.Metadata["x-amz-meta-title"];
+                    var filename = response.Metadata["x-amz-meta-filename"];
+                    var contentType = response.Headers["Content-Type"];
+
+                    Console.WriteLine($"Object meta, Title: {title}");
+                    Console.WriteLine($"Content type, Title: {contentType}");
+                    var responseBody = reader.ReadToEnd();
+                }
+            }
+
+
+            return Ok();
+        }
+
+        [HttpPost]
         public async Task<IActionResult> Post()
         {
             var files = Request.Form.Files;
             var size = files.Sum(f => f.Length);
 
             var clientId = Guid.NewGuid().ToString();
-            
+
             foreach (var file in files)
             {
                 using (var client = new AmazonS3Client(RegionEndpoint.USEast1))
@@ -48,7 +80,7 @@ namespace S3BucketFileUpload.Controllers
                             BucketName = "ydkaws.com",
                             CannedACL = S3CannedACL.PublicRead,
                         };
-                        
+
                         uploadRequest.Metadata.Add("FileName", file.FileName);
 
                         var fileTransferUtility = new TransferUtility(client);
